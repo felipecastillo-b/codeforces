653f3189332de079698ba3d8d85c6e5fbbf4e37e
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-03 01:18:36 -0400

    467A in java

---
+++ 800/467A_George_and_Accommodation.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        // t = in.nextInt(); // multiple cases
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
+        int rooms = 0;
+
+        for (int i = 0; i < n; i++) {
+            int p = in.nextInt();
+            int q = in.nextInt();
+
+            if (p < q-1) {
+                rooms++;
+            }
+        }
+
+        out.println(rooms);
+    }
+
+    // Fast I/O
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble() { return Double.parseDouble(next()); }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
