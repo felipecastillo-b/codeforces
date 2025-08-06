76c31fb4a277ff22d56a7f982eb64145330fd457
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-06 18:21:59 -0400

    1328A in java

---
+++ 800/1328A_Divisibility_Problem.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+        int t = in.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int r = a % b;
+
+            // if (r == 0) {
+            //     out.println(0);
+            // } else {
+            //     out.println(b - r);
+            // }
+
+            out.println(r == 0 ? 0 : b - r);
+        }
+    }
+
+    // Fast I/O
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
