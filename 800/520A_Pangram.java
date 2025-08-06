ec55922705fdf8c8367480caa9507a42a8a3d0ed
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-06 18:44:37 -0400

    520A in java

---
+++ 800/520A_Pangram.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
+        String s = in.nextLine().toLowerCase();
+        Set<Character> letters = new HashSet<>();
+
+        if (n < 26) {
+            out.println("NO");
+            return;
+        }
+
+        for (char c : s.toCharArray()) {
+            letters.add(c);
+        }
+
+        out.println(letters.size() >= 26 ? "YES" : "NO");
+    }
+
+    // Fast I/O
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
