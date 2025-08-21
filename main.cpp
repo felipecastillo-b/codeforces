1ae256dd21df8ba339ac8b9306c1d4917d4fe359
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-20 23:49:58 -0400

    cpp and template cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    fast_io;
+
+    int t; cin >> t;
+
+    while (t--) {
+        // solve
+    }
+    return 0;
+}
